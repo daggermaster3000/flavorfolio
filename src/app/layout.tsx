83d815c727210970b4133dfe52c7c3e4d3a91c26
daggermaster3000 <quillan.favey@gmail.com>
2025-09-01 23:42:18 +0200

    meta pwa stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,15 @@ export default function RootLayout({
       <head>
         <meta name="theme-color" content="#000000" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-      </head>
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Flavorfolio" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon.png" />     
       <link rel="manifest" href="/manifest.json" />
+     </head>
+      
       
 
       <body
